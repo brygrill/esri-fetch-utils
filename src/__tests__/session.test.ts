ca9e8f76c84a0274e9c4ef26f7c386930d6b3896
Bryan Grill <grillb@gmail.com>
2020-04-17 16:01:26 -0400

    update tests

--- src/__tests__/session.test.ts
+++ src/__tests__/session.test.ts
@@ -10,29 +10,49 @@ const customConfig = {
   redirectUri: 'http://localhost:3000/auth',
   portalUrl: 'http://myportal.com',
   tokenDuration: 1000,
-  state: 'mystate=9876',
+  state: 'qyxmpg9e5uWUPbxw',
 };
+
+const expectedUser = {
+  username: 'grill',
+  portalUrl: 'https://www.arcgis.com',
+  clientId: '1234',
+  token: '2YotnFZFEjr1zCsicMWpAA',
+  tokenDuration: 20160,
+  // tokenExpires: 'moment', this date will always change
+  state: 'qyxmpg9e5uWUPbxw',
+};
+
+beforeAll(() => {
+  window.location.hash =
+    '#access_token=2YotnFZFEjr1zCsicMWpAA&expires_in=3600&username=grill&state=qyxmpg9e5uWUPbxw';
+});
 
 test('session - browser - defaults', () => {
   const session = browserSession.create(defaultConfig);
-  console.log(session);
-  const user = session.user();
-  if (!user) {
-    console.log('show login');
-  }
+
   const oAuthUrl = session.oAuthUrl();
-  console.log(user);
   expect(oAuthUrl).toBe(
     'https://www.arcgis.com/sharing/rest/oauth2/authorize?client_id=1234&response_type=token&expiration=20160&redirect_uri=http://localhost:3000/auth&state=',
   );
+
+  const expires = session.tokenExpired(20160);
+  expect(expires).toBe(true);
+
+  const user = session.onRedirect();
+  expect(user).toMatchObject(expectedUser);
+  expect(user.tokenExpires).toBeTruthy();
 });
 
 test('session - browser - custom', () => {
   const session = browserSession.create(customConfig);
 
   const oAuthUrl = session.oAuthUrl();
-
   expect(oAuthUrl).toBe(
-    'http://myportal.com/sharing/rest/oauth2/authorize?client_id=1234&response_type=token&expiration=1000&redirect_uri=http://localhost:3000/auth&state=mystate=9876',
+    'http://myportal.com/sharing/rest/oauth2/authorize?client_id=1234&response_type=token&expiration=1000&redirect_uri=http://localhost:3000/auth&state=qyxmpg9e5uWUPbxw',
   );
+
+  const user = session.onRedirect();
+  expect(user.portalUrl).toBe(customConfig.portalUrl);
+  expect(user.tokenDuration).toBe(1000);
 });
